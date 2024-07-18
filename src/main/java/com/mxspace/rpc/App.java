85536ec90e16984ec70d5f179705111e53d7a5f6
zwq <zwq@202308>
2024-07-18 20:00:36 +0800

    添加说明

--- src/main/java/com/mxspace/rpc/App.java
+++ src/main/java/com/mxspace/rpc/App.java
@@ -2,11 +2,12 @@ package com.mxspace.rpc;
 
 import com.mxspace.rpc.annotation.EnableMxRpcClient;
 import com.mxspace.rpc.annotation.EnableMxRpcServer;
+import com.mxspace.rpc.enums.ProviderVisitStrategyEnums;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableMxRpcClient
-@EnableMxRpcServer
+@EnableMxRpcServer(visitStrategy = ProviderVisitStrategyEnums.NEXT)
 @SpringBootApplication
 public class App {
 
--- src/main/java/com/mxspace/rpc/component/MxRpcBeanPostProcessor.java
+++ src/main/java/com/mxspace/rpc/component/MxRpcBeanPostProcessor.java
@@ -11,14 +11,6 @@ import org.springframework.stereotype.Component;
 import javax.annotation.Resource;
 import java.lang.reflect.Field;
 
-/**
- * Netty客户端Bean后置处理器
- * 实现Spring后置处理器接口：BeanPostProcessor
- * 在Bean对象在实例化和依赖注入完毕后，在显示调用初始化方法的前后添加我们自己的逻辑。注意是Bean实例化完毕后及依赖注入完成后触发的
- *
- * @author ZC
- * @date 2021/3/2 23:00
- */
 @Component
 @Slf4j
 public class MxRpcBeanPostProcessor implements BeanPostProcessor {
@@ -56,7 +48,6 @@ public class MxRpcBeanPostProcessor implements BeanPostProcessor {
                         Object o = new MxRpcFactoryBean(type,this.mxRpcClientService).getObject();
                         //将代理类注入该字段
                         field.set(bean,o);
-                        log.info("创建代理类 ===>>> {}", beanName);
                     } catch (IllegalAccessException e) {
                         log.error(e.getMessage());
                     } catch (Exception e) {
